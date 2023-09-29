4a088158175237a4106cd01a27cfc2639213f570
Sarah Firyal <sarah5firyal@gmail.com>
2023-09-29 23:56:55 +0700

    style: [SF] dashboard page

--- src/components/Authentication/LoginComponent.vue
+++ src/components/Authentication/LoginComponent.vue
@@ -16,7 +16,7 @@
                             <div style="height: 100%;">
                                 <div id="close-button" @click="dialog = false"
                                     class="pa-10 d-flex align-center hover-pointer" style="width: fit-content;">
-                                    <v-icon icon="mdi-close" size="large" color="white" />
+                                    <v-icon color="white">mdi-close</v-icon>
                                     <p class="label-size font-weight-medium white--text ml-3 mb-1">
                                         Tutup
                                     </p>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,62 +1,100 @@
 <template>
   <v-container>
-    <div style="height: 20rem; position: relative" class="my-1">
-      <div
-        class="w-100 bg-orange-gradient rounded-lg d-flex flex-column justify-center align-center text-center"
-        style="height: 18rem; position: relative"
-      >
-        <h2 class="subheading-size white--text font-weight-medium">
+    <div style="height: 20rem; position: relative" class="mt-1">
+      <div class="w-100 bg-orange-gradient rounded-lg d-flex flex-column justify-center align-center text-center"
+        style="height: 18rem; position: relative">
+        <h2 class="subheading-size white--text font-weight-bold">
           Mulai kembangkan sekarang
         </h2>
         <p class="white--text label-size mt-2" style="max-width: 20rem">
           Bergabung dan dapatkan efisiensi kecepatan bisnis Anda.
         </p>
-        <img
-          :src="require(`@/assets/bg-dashboard.svg`)"
-          alt="Background"
-          class="background-image"
-        />
+        <img :src="require(`@/assets/bg-dashboard.svg`)" alt="Background"
+          style="position: absolute; left: 0; right: 0; margin: auto;" />
       </div>
-      <v-card
-        style="height: 4rem; max-width: 35rem"
-        class="search-card"
-        elevation="0"
-      ></v-card>
+      <v-card style="height: 4rem; max-width: 35rem" class="search-card" outlined></v-card>
     </div>
-    <v-row class="mt-10">
-      <v-slide-group class="w-100">
-        <v-slide-item v-for="data in investors" :key="data.uid">
-          <ServiceCardComponent
-            :data="data"
-            :total="data['totalInvestment']"
-            :type="data['typeData']"
-            class="mr-5"
-          />
-        </v-slide-item>
-      </v-slide-group>
-    </v-row>
-    <v-row class="mt-10">
-      <v-slide-group class="w-100" show-arrows="never">
-        <v-slide-item v-for="data in lawFirms" :key="data.uid">
-          <ServiceCardComponent
-            :data="data"
-            :total="data['totalClient']"
-            :type="data['typeData']"
-            class="mr-5"
-          />
-        </v-slide-item>
-      </v-slide-group>
-    </v-row>
+
+    <div class="mt-16 pt-4">
+      <v-row class="align-center">
+        <h6 class="label-size font-weight-bold mr-10 mb-1">
+          Rekomendasi Investor
+        </h6>
+        <div class="d-flex align-center">
+          <p class="big-body-size primary--text font-weight-bold ma-0 mb-1 mr-1 hover-pointer">Lihat Semua</p>
+          <v-icon class="primary--text">mdi-chevron-right</v-icon>
+        </div>
+      </v-row>
+      <v-row class="mt-10 align-center" style="position: relative; overflow: hidden;">
+        <TitleCardComponent category="Investor" />
+        <v-slide-group class="w-100" style="height: 22rem; position: absolute; left: 16rem;">
+          <div class="d-flex" style="gap: 0.4rem;">
+            <v-slide-item v-for="data in investors" :key="data.uid">
+              <ServiceCardComponent :data="data" :total="data['totalInvestment']" :type="data['typeData']" />
+            </v-slide-item>
+          </div>
+        </v-slide-group>
+      </v-row>
+    </div>
+
+    <div class="mt-16 pt-4">
+      <v-row class="align-center">
+        <h6 class="label-size font-weight-bold mr-10 mb-1">
+          Rekomendasi Inovator
+        </h6>
+        <div class="d-flex align-center">
+          <p class="big-body-size primary--text font-weight-bold ma-0 mb-1 mr-1 hover-pointer">Lihat Semua</p>
+          <v-icon class="primary--text">mdi-chevron-right</v-icon>
+        </div>
+      </v-row>
+      <v-row class="mt-10 align-center" style="position: relative; overflow: hidden;">
+        <TitleCardComponent category="Inovator" />
+        <v-slide-group class="w-100" style="height: 22rem; position: absolute; left: 16rem;">
+          <div class="d-flex" style="gap: 0.4rem;">
+            <v-slide-item v-for="data in lawFirms" :key="data.uid">
+              <ServiceCardComponent :data="data" :total="data['totalClient']" :type="data['typeData']" />
+            </v-slide-item>
+          </div>
+        </v-slide-group>
+      </v-row>
+    </div>
+
+    <div class="mt-16 pt-4 mb-12">
+      <v-row class="align-center">
+        <h6 class="label-size font-weight-bold mr-10 mb-1">
+          Rekomendasi Jasa Legalitas
+        </h6>
+        <div class="d-flex align-center">
+          <p class="big-body-size primary--text font-weight-bold ma-0 mb-1 mr-1 hover-pointer">Lihat Semua</p>
+          <v-icon class="primary--text">mdi-chevron-right</v-icon>
+        </div>
+      </v-row>
+      <v-row class="mt-10 align-center" style="position: relative; overflow: hidden;">
+        <TitleCardComponent category="Law Firm" />
+        <v-slide-group class="w-100" style="height: 22rem; position: absolute; left: 16rem;">
+          <div class="d-flex" style="gap: 0.4rem;">
+            <v-slide-item v-for="data in lawFirms" :key="data.uid">
+              <ServiceCardComponent :data="data" :total="data['totalClient']" :type="data['typeData']" />
+            </v-slide-item>
+          </div>
+        </v-slide-group>
+      </v-row>
+    </div>
+
+
   </v-container>
 </template>
+
 <script>
 import ServiceCardComponent from "../components/Common/ServiceCardComponent.vue";
+import TitleCardComponent from "../components/Common/TitleCardComponent.vue";
 import { getInvestors, getLawFirms } from "../db/dataRepository.js";
 
 export default {
   name: "DashboardView",
   components: {
     ServiceCardComponent,
+    TitleCardComponent,
   },
   data: () => ({
     innovators: [],
@@ -70,20 +108,10 @@ export default {
 };
 </script>
 
-
 <style>
 .v-slide-group .v-slide-group__next,
 .v-slide-group .v-slide-group__prev {
   display: none;
-}
-
-.background-image {
-  z-index: -50;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  right: 0;
-  margin: auto;
 }
 
 .search-card {
