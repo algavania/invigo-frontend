e23231e69b2138c020d155a4e59b8456f58be5bb
Sarah Firyal <sarah5firyal@gmail.com>
2023-09-30 21:42:21 +0700

    style: [SF] add document legal dialog

--- src/views/InovatorProfileView.vue
+++ src/views/InovatorProfileView.vue
@@ -36,101 +36,80 @@
                 </div>
 
                 <v-form ref="form">
-                    <v-text-field
-                      label="Nama"
-                      type="text"
-                      v-model="name"
-                      outlined
-                      dense
-                    />
-                    <v-text-field
-                      label="Username"
-                      type="text"
-                      v-model="username"
-                      outlined
-                      dense
-                    />
-                    <v-text-field
-                      label="Email"
-                      type="email"
-                      v-model="email"
-                      outlined
-                      dense
-                      disabled
-                    />
+                    <v-text-field label="Nama" type="text" v-model="name" outlined dense />
+                    <v-text-field label="Username" type="text" v-model="username" outlined dense />
+                    <v-text-field label="Email" type="email" v-model="email" outlined dense disabled />
 
                     <h6 class="label-size font-weight-bold my-5">
                         Tentang
                     </h6>
-                    <v-text-field
-                      label="Alamat"
-                      type="text"
-                      v-model="address"
-                      outlined
-                      dense
-                    />
-                    <v-text-field
-                      label="Jumlah Investor"
-                      type="text"
-                      v-model="investorAmount"
-                      outlined
-                      dense
-                    />
-                    <v-text-field
-                      label="Website"
-                      type="text"
-                      v-model="website"
-                      hint="https://"
-                      outlined
-                      dense
-                    />
-                    <v-select
-                        label="Industri"
-                        :items="['Option 1', 'Option 2', 'Option 3']"
-                        outlined
-                        dense
-                        multiple
-                    />
-                    <v-text-field
-                      label="Penemu"
-                      type="text"
-                      v-model="founder"
-                      outlined
-                      dense
-                    />
+                    <v-text-field label="Alamat" type="text" v-model="address" outlined dense />
+                    <v-text-field label="Jumlah Investor" type="text" v-model="investorAmount" outlined dense />
+                    <v-text-field label="Website" type="text" v-model="website" hint="https://" outlined dense />
+                    <v-select label="Industri" :items="['Option 1', 'Option 2', 'Option 3']" outlined dense multiple />
+                    <v-text-field label="Penemu" type="text" v-model="founder" outlined dense />
 
                     <h6 class="label-size font-weight-bold my-5">
                         Detail
                     </h6>
-                    <v-select
-                        label="Tipe Investasi"
-                        :items="['Option 1', 'Option 2', 'Option 3']"
-                        outlined
-                        dense
-                        multiple
-                    />
-                    <v-select
-                        label="Tahap Investasi"
-                        :items="['Option 1', 'Option 2', 'Option 3']"
-                        outlined
-                        dense
-                        multiple
-                    />
+                    <v-select label="Tipe Investasi" :items="['Option 1', 'Option 2', 'Option 3']" outlined dense
+                        multiple />
+                    <v-select label="Tahap Investasi" :items="['Option 1', 'Option 2', 'Option 3']" outlined dense
+                        multiple />
 
                     <h6 class="label-size font-weight-bold my-5">
                         Inovasi Saya
                     </h6>
-                    <v-text-field
-                      label="Deskripsi"
-                      type="text"
-                      v-model="inovationDescription"
-                      outlined
-                      dense
-                    />
+                    <v-text-field label="Deskripsi" type="text" v-model="inovationDescription" outlined dense />
                     <FileUploadComponent label="Proposal" description="Dokumen PDF dengan ukuran maksimal 5mb." />
                     <FileUploadComponent label="Pitch Deck" description="Dokumen PDF dengan ukuran maksimal 5mb." />
-                    <FileUploadComponent label="Business Modal Canvas" description="Dokumen PDF dengan ukuran maksimal 5mb." />
+                    <FileUploadComponent label="Business Modal Canvas"
+                        description="Dokumen PDF dengan ukuran maksimal 5mb." />
                     <FileUploadComponent label="Demo Produk" description="" />
+
+                    <div class="d-flex align-center">
+                        <p class="font-weight-bold ma-0 flex-grow-1">Dokumen Legal</p>
+                        <v-dialog v-model="dialog" width="500">
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn icon v-bind="attrs" v-on="on">
+                                    <v-icon color="black" style="font-size: 1.8rem;">mdi-plus</v-icon>
+                                </v-btn>
+                            </template>
+
+                            <v-card>
+                                <v-card-title>
+                                    <div class="d-flex align-center" style="width: 100%;">
+                                        <p class="font-weight-bold ma-0 flex-grow-1">Tambah Dokumen</p>
+                                        <v-btn icon v-bind="attrs" v-on="on" @click="dialog = false">
+                                            <v-icon class="gray500--text" style="font-size: 1.8rem;">mdi-close</v-icon>
+                                        </v-btn>
+                                    </div>
+                                </v-card-title>
+
+                                <v-card-text class="mt-6 pb-0">
+                                    <v-select v-model="uploadType" label="Tipe Unggahan" :items="['Dokumen', 'Tautan']" outlined dense class="mb-2" />
+                                    <div v-if="uploadType == 'Dokumen'">
+                                        <v-text-field label="Judul" type="text" v-model="documentTitle" outlined dense />
+                                        <FileUploadComponent label="Dokumen" description="Dokumen PDF dengan ukuran maksimal 5mb." />
+                                    </div>
+                                    <div v-if="uploadType == 'Tautan'">
+                                        <v-text-field label="Judul" type="text" v-model="documentTitle" outlined dense />
+                                        <v-text-field label="URL Tautan" type="text" v-model="documentURL" hint="https://" outlined dense />
+                                    </div>
+                                </v-card-text>
+
+                                <v-card-actions v-if="uploadType != ''">
+                                    <v-btn color="primary" height="50" elevation="0" class="flex-grow-1 mb-2" style="width: 100%;">
+                                        Simpan
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+
+                    </div>
+                    <div>
+
+                    </div>
 
                     <div class="d-flex mt-6" style="gap: 1rem;">
                         <v-btn color="primary" height="50" elevation="0" class="flex-grow-1">
@@ -152,9 +131,11 @@ import FileUploadComponent from "../components/Common/FileUploadComponent.vue"
 export default {
     name: "InovatorProfileView",
     components: {
-        FileUploadComponent
+        FileUploadComponent,
     },
     data: () => ({
+        dialog: false,
+        uploadType: "",
         items: [
             {
                 text: "Beranda",
